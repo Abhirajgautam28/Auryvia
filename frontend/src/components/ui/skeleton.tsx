ec6b35d5454b1534cc644e89f1d0d0da1b6140e0
ABHIRAJ GAUTAM <abhirajgautam28@gmail.com>
2025-09-11 19:18:39 +0530

    Add skeleton component for loading states.

---
+++ frontend/src/components/ui/skeleton.tsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+import { cn } from "@/lib/utils";
+
+export interface SkeletonProps extends React.HTMLAttributes<HTMLDivElement> {}
+
+export const Skeleton = React.forwardRef<HTMLDivElement, SkeletonProps>(
+  ({ className, ...props }, ref) => (
+    <div
+      ref={ref}
+      className={cn(
+        "animate-pulse bg-slate-700/60 rounded-md",
+        className
+      )}
+      {...props}
+    />
+  )
+);
+Skeleton.displayName = "Skeleton";
